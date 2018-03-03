7f24a0e759b30ec903b70a406ebc7f5816e0124c
Jevape <Jevape@JEVAPE-PC>
2018-03-03 16:41:05 +0100

    Primer commit

---
+++ T4_ED_Pactica/src/es/studium/T4/Centros.java
@@ -0,0 +1,24 @@
+package es.studium.T4;
+
+public class Centros {
+	private String nombre;
+	private String direccion;
+	private String alumnos;
+	private int numerohoras;
+	private int idCentro;
+	public Centros() {
+	nombre = "";
+	direccion = "";
+	alumnos = "";
+	numerohoras = 0;
+	idCentro=0;
+	}
+	public Centros (String n, String d, String a, int nh,int ic) {
+	nombre = n;
+	direccion = d;
+	alumnos = a;
+	numerohoras = nh;
+	idCentro= ic;
+	}
+
+}
